8fe779adc0eb5e15c6d8210629f6bb84dc5ba730
njordan <njordan1@uw.edu>
2025-05-14 11:21:03 -0700

    Finished RNN model

--- RNN.py
+++ RNN.py
@@ -1,11 +1,8 @@
+from keras import Input
+from keras.api.models import Sequential
+from keras.api.layers import TextVectorization, Embedding, LSTM, Dense, Dropout
 from keras.api.preprocessing import text_dataset_from_directory
 from tensorflow._api.v2.strings import regex_replace
-from keras.api.models import Sequential
-from keras import Input
-from keras.api.layers import TextVectorization
-
-
-
 
 
 # Prepares data for training and testing
@@ -26,13 +23,13 @@ model.add(Input(shape=(1,), dtype="string"))
 
 # Defining TextVectorization layer
 # Converts input text into vectors for embedding databse
-max_tokens = 1000
-max_len = 100
+max_tokens = 2000
+max_len = 175
 vectorization_layer = TextVectorization(
     # Max words processed at once
     max_tokens=max_tokens,
     # Output integer indices
-    output_mode=int,
+    output_mode="int",
     # Consistent output shape
     output_sequence_length=max_len,
     )
@@ -42,5 +39,34 @@ train_texts = train_data.map(lambda text, label: text)
 vectorization_layer.adapt(train_texts)
 
 model.add(vectorization_layer)
+
+# Embedding layer (fixed length vectors)
+model.add(Embedding(max_tokens + 1, 128))
+
+# Recurrent Layer
+model.add(LSTM(64))
+
+model.add(Dropout(0.5))
+
+# Dense layer for boolean output via sigmoid function
+model.add(Dense(64, activation="relu"))
+model.add(Dense(1, activation="sigmoid"))
+
+
+# Compile model
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=['accuracy'],
+)
+
+# Train model
+model.fit(train_data, epochs=10)
+
+# Save weights
+# model.save_weights('cnn.h5')
+
+# # Load weights
+# model.load_weights('cnn.h5')
 
 
